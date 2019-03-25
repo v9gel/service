c6cb874bf880bc8fed9a2ecf94afb1b1070d4b94
Vyacheslav <v9gel@mail.ru>
2019-03-25 15:30:00 +0700

    Включен CORS

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ models.sequelize.sync().then(function() {
 
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
