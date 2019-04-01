57b2799b493dda6e4178e519d9613d21615b3bab
Vyacheslav <v9gel@mail.ru>
2019-04-02 01:02:05 +0700

    Логин

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser")
 
 var indexRouter = require('./routes/index');
 var referenncesRouter = require('./routes/references');
+var loginRouter = require('./routes/login');
 var cors = require('cors');
 
 var app = express();
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/references', referenncesRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
