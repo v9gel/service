812a64dffe00b30c12ee6ef1f0c1e477415a51f2
Вячеслав Нагель <v9gel@mail.ru>
2019-04-07 11:32:10 +0700

    Вынесены переменные окружения

--- models/index.js
+++ models/index.js
@@ -3,25 +3,29 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var env = "development";
-var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  { host: process.env.HOST, dialect: "mysql" }
+);
 var db = {};
 
-fs
-    .readdirSync(__dirname)
-    .filter(function(file) {
-        return (file.indexOf(".") !== 0) && (file !== "index.js") && (file !== "generate.js");
-    })
-    .forEach(function(file) {
-        var model = sequelize.import(path.join(__dirname, file));
-        db[model.name] = model;
-    });
+fs.readdirSync(__dirname)
+  .filter(function(file) {
+    return (
+      file.indexOf(".") !== 0 && file !== "index.js" && file !== "generate.js"
+    );
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(function(modelName) {
-    if ("associate" in db[modelName]) {
-        db[modelName].associate(db);
-    }
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
